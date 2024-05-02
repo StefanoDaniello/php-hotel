d84b4975c9b5560528d072360b1d8f005ef3d682
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-05-02 14:55:55 +0200

    start

---
+++ Views/table.php
@@ -0,0 +1,23 @@
+<table class="table table-striped">
+    <thead>
+      <tr>
+        <th scope="col">name</th>
+        <th scope="col">description</th>
+        <th scope="col">parking</th>
+        <th scope="col">vote</th>
+        <th scope="col">distance to center</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+      foreach ($hotels as $hotel) {
+        echo "<tr> <td>{$hotel['name']}</td> <td>{$hotel['description']}</td> 
+        <td>{$hotel['parking']}</td> 
+        <td>{$hotel['vote']}</td> 
+        <td>{$hotel['distance_to_center']}</td>
+        </tr>";
+        }
+      ?>
+
+    </tbody>
+</table>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+include __DIR__ . "/Models/hotel.php";
+
+include __DIR__ . "/Views/header.php";
+?>
+<main class="container">
+  <?php
+    include __DIR__ . "/Views/table.php";
+  ?>
+</main>
